9d6f0c7e60268d8eebfd8a27c035cbfb3e6b9769
HienTran2309 <tranthehien.dev@gmail.com>
2026-03-24 17:37:26 +0700

    chuan bi deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,19 +3,31 @@ import tasksRouter from './routes/tasksRouters.js';
 import { connectDB } from "./config/db.js";
 import dotenv from 'dotenv';
 import cors from "cors";
+import path from 'path'
 
 dotenv.config();
 
-const PORT= process.env.PORT || 5001
+const PORT = process.env.PORT || 5001
+
+const __dirname = path.resolve();
 
 const app = express();
 
 app.use(express.json());
 
 //Middlewares
-app.use(cors({origin:"http://localhost:5173"}));
-
+if(process.env.NODE_ENV !=="production"){
+    app.use(cors({ origin: "http://localhost:5173" }));
+}
 app.use("/api/tasks", tasksRouter)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+    });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
